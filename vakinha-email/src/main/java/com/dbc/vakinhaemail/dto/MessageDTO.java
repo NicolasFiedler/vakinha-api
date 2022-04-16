f60d41e0354cb6964cd3769441aa4fd2e8a93191
Gabriel Poersch <gabpoersch@gmail.com>
2022-04-16 04:58:12 -0300

    base vakinha email (com erro ainda)

---
+++ vakinha-email/src/main/java/com/dbc/vakinhaemail/dto/MessageDTO.java
@@ -0,0 +1,19 @@
+package com.dbc.vakinhaemail.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class MessageDTO {
+    private String sender;
+    private List<String> receiver;
+    private String subject;
+    private String text;
+}
---
+++ vakinha-email/src/main/java/com/dbc/vakinhaemail/service/MailService.java
@@ -0,0 +1,27 @@
+package com.dbc.vakinhaemail.service;
+
+import com.dbc.vakinhaemail.dto.MessageDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MailService {
+
+    @Autowired
+    private JavaMailSender javaMailSender;
+
+    public void enviar(MessageDTO messageDTO) {
+        SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
+
+        simpleMailMessage.setFrom(messageDTO.getSender());
+        simpleMailMessage.setTo(messageDTO.getReceiver()
+                .toArray(new String[messageDTO.getReceiver().size()]));
+        simpleMailMessage.setSubject(messageDTO.getSubject());
+        simpleMailMessage.setText(messageDTO.getText());
+
+        javaMailSender.send(simpleMailMessage);
+    }
+
+}
